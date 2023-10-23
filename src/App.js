177815bbaaee40dbfec2270b1d78622a40b2d1f5
ABDALRHMAN <abdalrhmanahmed00004@gmail.com>
2023-10-23 04:15:30 +0300

    all liberaries, pages setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Productlist from './components/productlist';
+
 
 function App() {
   return (
     <div className="App">
-      <Productlist />
+      
     </div>
   );
 }
