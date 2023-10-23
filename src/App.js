b05e32517558f6744eab774c6028f0e283e1f34a
ABDALRHMAN <abdalrhmanahmed00004@gmail.com>
2023-10-23 04:44:24 +0300

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
+import { Route, Router } from 'react-router-dom';
+
+//Pages
+import Navbar from './components/nav';
 
 
 function App() {
   return (
     <div className="App">
-      
+      <>
+        <Navbar/>
+        <div className='row'>
+          <div className='col-2'>
+            test
+          </div>
+          <div className='col-10'>
+            test2
+          </div>
+        </div>
+      </>
     </div>
   );
 }
